5e51acc4839fa63f58c0ee60c25fbc1acd11a210
ShroukOuda <s36568961@gmail.com>
2025-06-30 03:30:18 +0300

    Create many-to-many relationship: BookAuthor

---
+++ Models/BookAuthor.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.Models
+{
+    public class BookAuthor
+    {
+        public int BookId { get; set; }
+        public int AuthorId { get; set; }
+
+        //Navigation Properties
+        public Book Book { get; set; }
+        public Author Author { get; set; }
+    }
+}
