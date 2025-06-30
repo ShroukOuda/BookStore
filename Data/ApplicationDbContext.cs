a6fd387f1909249934ae729267acc00626fd35ae
ShroukOuda <s36568961@gmail.com>
2025-06-30 03:27:36 +0300

    Add ApplicationDbContext and connect SQL Server

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+
+        }
+
+        DbSet<Book> Books { get; set; }
+        DbSet<Author> Authors { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<BookAuthor> BookAuthors { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
