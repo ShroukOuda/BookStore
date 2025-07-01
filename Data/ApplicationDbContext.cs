9d298c2b854ae1cdc3a782dd7dad5ffd828e41d1
ShroukOuda <s36568961@gmail.com>
2025-07-02 01:21:08 +0300

    Configure composite key for BookAuthor in OnModelCreating

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,8 +5,7 @@
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-            
-
+           
         }
 
         DbSet<Book> Books { get; set; }
@@ -15,7 +14,21 @@
         DbSet<BookAuthor> BookAuthors { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<BookAuthor>()
+                .HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ba => ba.BookId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Author)
+                .WithMany(a => a.BookAuthors)
+                .HasForeignKey(ba => ba.AuthorId);
             base.OnModelCreating(modelBuilder);
+
+            
         }
     }
 }
