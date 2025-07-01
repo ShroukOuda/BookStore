73acac9ff864f5a1260eb6e6a3a22837735919e8
ShroukOuda <s36568961@gmail.com>
2025-07-02 01:25:13 +0300

    Register AuthorSeeder, CategorySeeder, BookSeeder and BookAuthorSeeder in ModelBuilderExtensions

---
+++ Data/Seed/ModelBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.Data.Seed
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            AuthorSeeder.Seed(modelBuilder);
+            BookSeeder.Seed(modelBuilder);
+            CategorySeeder.Seed(modelBuilder);
+            BookAuthorSeeder.Seed(modelBuilder);
+        }
+    }
+    
+}
