c7637c3e88458cd6fea2450c94b0ee99078a5c77
ShroukOuda <s36568961@gmail.com>
2025-07-04 04:13:07 +0300

    Add UpdateBookIsbn Migrations

---
+++ Migrations/20250704011105_UpdateBookIsbn.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookIsbn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ISBN",
+                value: "9780135264027");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ISBN",
+                value: "9781617295071");
+        }
+    }
+}
