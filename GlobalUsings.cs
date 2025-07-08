2bd5f89203068fa34e3b2324a1177090fb4a83a4
ShroukOuda <s36568961@gmail.com>
2025-07-08 18:13:14 +0300

    add global using BookStore.Data

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,4 +3,5 @@ global using BookStore.Models;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using BookStore.Data.Seed;
+global using BookStore.Data;
 
