e2faa938fa121de59aa9f37fb70f41bfaba156be
ShroukOuda <s36568961@gmail.com>
2025-07-08 18:08:47 +0300

    Add BooksController with Index and Create methods

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+	public class BooksController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public BooksController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var books = _context.Books
+				.Include(c => c.Category)
+				.Include(b => b.BookAuthors)
+					.ThenInclude(ba => ba.Author)
+				.ToList();
+			return View(books);
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+	}
+}
