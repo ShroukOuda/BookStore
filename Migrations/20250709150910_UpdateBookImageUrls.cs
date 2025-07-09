51023eda924f59aedcd497697c978d9255eb1b37
ShroukOuda <s36568961@gmail.com>
2025-07-09 18:44:59 +0300

    Add UpdateBookImageUrls Migrations

---
+++ Migrations/20250709150910_UpdateBookImageUrls.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookImageUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "clean-code.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "html-css.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "ydkjs-scope.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "cracking-coding.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "pragmatic-programmer.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "head-first-patterns.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "js-good-parts.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "react-native.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImageUrl",
+                value: "learning-sql.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "phoenix-project.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "refactoring.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageUrl",
+                value: "ios-swift.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "/images/books/clean-code.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "/images/books/html-css.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "/images/books/ydkjs-scope.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "/images/books/cracking-coding.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "/images/books/pragmatic-programmer.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "/images/books/head-first-patterns.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "/images/books/js-good-parts.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "/images/books/react-native.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImageUrl",
+                value: "/images/books/learning-sql.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "/images/books/phoenix-project.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "/images/books/refactoring.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageUrl",
+                value: "/images/books/ios-swift.jpg");
+        }
+    }
+}
