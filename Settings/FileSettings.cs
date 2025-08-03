0a999e75f3297da1b2c0b2ca982cb4a5bd227dc7
ShroukOuda <s36568961@gmail.com>
2025-08-03 09:02:21 +0300

    add settings

---
+++ Settings/FileSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Settings
+{
+    public static class FileSettings
+    {
+        public const string ImagesPath = "images/books";
+        public const string AllowedExtensions = ".jpg,.jpeg,.png";
+        public const int MaxFileSizeInMB = 5;
+        public const int MaxFileSizeInBytes = MaxFileSizeInMB * 1024 * 1024;
+    }
+}
