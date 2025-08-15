1b1455162ae80bb2e9621c31884a6db7897fedd6
ShroukOuda <s36568961@gmail.com>
2025-08-15 20:43:30 +0300

    update MaxFileSize

--- Settings/FileSettings.cs
+++ Settings/FileSettings.cs
@@ -4,7 +4,7 @@
     {
         public const string ImagesPath = "images/books";
         public const string AllowedExtensions = ".jpg,.jpeg,.png";
-        public const int MaxFileSizeInMB = 5;
+        public const int MaxFileSizeInMB = 1;
         public const int MaxFileSizeInBytes = MaxFileSizeInMB * 1024 * 1024;
     }
 }
